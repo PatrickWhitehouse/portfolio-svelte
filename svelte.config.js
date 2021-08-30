5f3735f92e043ec47d7ae52c3bc553c9785dbeca
Patrick Whitehouse <patrick1996@gmail.com>
2021-08-30 17:23:08 +0100

    update conifg

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from "@sveltejs/adapter-netlify"
 const config = {
     kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        adapter: adapter()
+    },
 
     preprocess: [preprocess({
         "postcss": true
